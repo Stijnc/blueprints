9bf4de1572847804e799eede349cb7acb16f01da
Mike Wasson <mwasson@microsoft.com>
2016-03-02 14:29:24 -0800

    Sample for multi VM in a single tier

---
+++ multivm-windows/azurecli-multi-vm-single-tier-sample.cmd
@@ -0,0 +1,151 @@
+:: This script was written for Azure CLI version 0.9.16 (pre-release)
+
+ECHO OFF
+SETLOCAL
+
+IF "%~1"=="" (
+    ECHO Usage: %0 subscription-id
+    EXIT /B
+    )
+
+:: Set up variables to build out the naming conventions for deploying
+:: the cluster
+
+SET LOCATION=eastus2
+SET APP_NAME=app1
+SET ENVIRONMENT=dev
+SET USERNAME=testuser
+SET PASSWORD=AweS0me@PW
+
+SET NUM_VM_INSTANCES=2
+
+:: Explicitly set the subscription to avoid confusion as to which subscription
+:: is active/default
+SET SUBSCRIPTION=%1
+
+:: Set up the names of things using recommended conventions
+SET RESOURCE_GROUP=%APP_NAME%-%ENVIRONMENT%-rg
+SET AVAILSET_NAME=%APP_NAME%-as
+
+SET LB_NAME=%APP_NAME%-lb
+SET LB_FRONTEND_NAME=%LB_NAME%-frontend
+SET LB_BACKEND_NAME=%LB_NAME%-backend-pool
+SET LB_PROBE_NAME=%LB_NAME%-probe
+SET IP_NAME=%APP_NAME%-pip
+SET SUBNET_NAME=%APP_NAME%-subnet
+SET VNET_NAME=%APP_NAME%-vnet
+SET DIAGNOSTICS_STORAGE=%APP_NAME:-=%diag
+
+:: For Windows, use the following command to get the list of URNs:
+:: azure vm image list %LOCATION% MicrosoftWindowsServer WindowsServer 2012-R2-Datacenter
+SET WINDOWS_BASE_IMAGE=MicrosoftWindowsServer:WindowsServer:2012-R2-Datacenter:4.0.20160126
+
+:: For a list of VM sizes see...
+SET VM_SIZE=Standard_DS1
+
+:: Set up the postfix variables attached to most CLI commands
+SET POSTFIX=--resource-group %RESOURCE_GROUP% --subscription %SUBSCRIPTION%
+
+CALL azure config mode arm
+
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: Create resources
+
+:: Create the enclosing resource group
+CALL azure group create --name %RESOURCE_GROUP% --location %LOCATION% ^
+  --subscription %SUBSCRIPTION%
+
+:: Create the availability set
+CALL azure availset create --name %AVAILSET_NAME% --location %LOCATION% %POSTFIX%
+
+:: Create the VNet
+CALL azure network vnet create --address-prefixes 10.0.0.0/16 ^
+  --name %VNET_NAME% --location %LOCATION% %POSTFIX%
+
+:: Create the subnet
+CALL azure network vnet subnet create --vnet-name %VNET_NAME% --address-prefix ^
+  10.0.0.0/24 --name %SUBNET_NAME% %POSTFIX%
+
+:: Create the public IP address (dynamic)
+CALL azure network public-ip create --name %IP_NAME% --location %LOCATION% %POSTFIX%
+
+:: Create the storage account for diagnostics logs
+CALL azure storage account create --type LRS --location %LOCATION% %POSTFIX% ^
+  %DIAGNOSTICS_STORAGE%
+
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: Load balancer
+
+:: Create the load balancer
+CALL azure network lb create --name %LB_NAME% --location %LOCATION% %POSTFIX%
+
+:: Create LB front-end and associate it with the public IP address
+CALL azure network lb frontend-ip create --name %LB_FRONTEND_NAME% --lb-name ^
+  %LB_NAME% --public-ip-name %IP_NAME% %POSTFIX%
+
+:: Create LB back-end address pool
+CALL azure network lb address-pool create --name %LB_BACKEND_NAME% --lb-name ^
+  %LB_NAME% %POSTFIX%
+
+:: Create a health probe for an HTTP endpoint
+CALL azure network lb probe create --name %LB_PROBE_NAME% --lb-name %LB_NAME% ^
+  --port 80 --interval 5 --count 2 --protocol http --path / %POSTFIX%
+
+:: Create a load balancer rule for HTTP
+CALL azure network lb rule create --name %LB_NAME%-rule-http --protocol tcp ^
+  --lb-name %LB_NAME% --frontend-port 80 --backend-port 80 --frontend-ip-name ^
+  %LB_FRONTEND_NAME% --probe-name %LB_PROBE_NAME% %POSTFIX%
+
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: Create VMs and per-VM resources
+FOR /L %%I IN (1,1,%NUM_VM_INSTANCES%) DO CALL :CreateVM %%I
+
+GOTO :eof
+
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: Subroutine to create the VMs and per-VM resources
+
+:CreateVm
+
+ECHO Creating VM %1
+
+SET VM_NAME=%APP_NAME%-vm%1
+SET NIC_NAME=%VM_NAME%-0nic
+SET VHD_STORAGE=%VM_NAME:-=%st0
+SET /a RDP_PORT=50001 + %1
+
+:: Create NIC for VM1
+CALL azure network nic create --name %NIC_NAME% --subnet-name %SUBNET_NAME% ^
+  --subnet-vnet-name %VNET_NAME% --location %LOCATION% %POSTFIX%
+
+:: Add NIC to back-end address pool
+CALL azure network nic address-pool add --name %NIC_NAME% --lb-name %LB_NAME% ^
+  --lb-address-pool-name %LB_BACKEND_NAME% %POSTFIX%
+
+:: Create NAT rule for RDP
+CALL azure network lb inbound-nat-rule create --name rdp-vm%1 --frontend-port ^
+  %RDP_PORT% --backend-port 3389 --lb-name %LB_NAME% --frontend-ip-name ^
+  %LB_FRONTEND_NAME% %POSTFIX%
+
+:: Add NAT rule to the NIC
+CALL azure network nic inbound-nat-rule add --name %NIC_NAME% --lb-name ^
+  %LB_NAME% --lb-inbound-nat-rule-name rdp-vm%1 %POSTFIX%
+
+:: Create the storage account for the OS VHD
+CALL azure storage account create --type PLRS --location %LOCATION% ^
+ %VHD_STORAGE% %POSTFIX%
+
+:: Create the VM
+CALL azure vm create --name %VM_NAME% --os-type Windows --image-urn ^
+  %WINDOWS_BASE_IMAGE% --vm-size %VM_SIZE% --vnet-subnet-name %SUBNET_NAME% ^
+  --nic-name %NIC_NAME% --vnet-name %VNET_NAME% --storage-account-name ^
+  %VHD_STORAGE% --os-disk-vhd "%VM_NAME%-osdisk.vhd" --admin-username ^
+  "%USERNAME%" --admin-password "%PASSWORD%" --boot-diagnostics-storage-uri ^
+  "https://%DIAGNOSTICS_STORAGE%.blob.core.windows.net/" --availset-name ^
+  %AVAILSET_NAME% --location %LOCATION% %POSTFIX%
+
+:: Attach a data disk
+CALL azure vm disk attach-new --vm-name %VM_NAME% --size-in-gb 128 --vhd-name ^
+  %VM_NAME%-data1.vhd --storage-account-name %VHD_STORAGE% %POSTFIX%
+
+goto :eof
